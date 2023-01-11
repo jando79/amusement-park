128f7caaa8cf5375c41980b60627e412b239b640
David Jandron <jandron1211@gmail.com>
2023-01-11 14:26:28 -0800

    add js event listeners to remove attributes based on conditions

---
+++ js/scripts.js
@@ -0,0 +1,35 @@
+// User Interface Logic
+
+function hideResults() {
+  document.getElementById("swings").setAttribute("class", "hidden");
+  document.getElementById("coaster").setAttribute("class", "hidden");
+  document.getElementById("tower").setAttribute("class", "hidden");
+  document.getElementById("sorry").setAttribute("class", "hidden");
+}
+
+window.onload = function() {
+  document.querySelector("form").onsubmit = function(event) {
+    event.preventDefault();
+    hideResults();
+    const age = parseInt(document.querySelector("input#age").value);
+    const height = parseInt(document.querySelector("input#height").value);
+     
+    // we're evaluating the Tower of Doom ride first
+     if (age >= 12 && height >= 60) {
+      document.getElementById("swings").removeAttribute("class");
+      document.getElementById("coaster").removeAttribute("class");
+      document.getElementById("tower").removeAttribute("class");
+  
+       // Next, we're evaluating the Roller Coaster ride.
+      } else if (age >= 12 || height >= 48) {
+      document.getElementById("swings").removeAttribute("class");
+      document.getElementById("coaster").removeAttribute("class");
+      
+      } else if (age >= 6) {
+      document.getElementById("swings").removeAttribute("class");
+      // Finally, we have our "catch-all" for when a user is less than 6 years old.
+      } else {
+      document.getElementById("sorry").removeAttribute("class");
+      }
+    };
+  };
